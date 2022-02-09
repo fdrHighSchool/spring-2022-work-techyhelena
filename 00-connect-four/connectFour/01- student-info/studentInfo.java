61f84da124aefba75184c910e0ad30fa91c3b5e1
Helen Zhou <fdr.helenzhou@gmail.com>
2022-02-08 22:41:14 -0500

    addition

--- 00-connect-four/connectFour/01- student-info/studentInfo.java
+++ 00-connect-four/connectFour/01- student-info/studentInfo.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class studentInfo {
     public static void main (String [][] args) {
+        //initiating a new array 
+        int [][] arr = new int[5][4];
         
-    }
-}
+    } // closes main 
+
+    
+
+
+} // closes class
