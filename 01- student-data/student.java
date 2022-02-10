8000e3aa775af27f8bccf20f54e5988a75fa1c57
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2022-02-10 08:47:23 -0500

    constructor methods

--- 01- student-data/student.java
+++ 01- student-data/student.java
@@ -4,4 +4,36 @@ public class student {
   private String name;
   private boolean closeContact;
   private int[] grades;
+
+  // constructor method
+  // used to set up the default values
+  public student(String id, String n, String cc, int [] g) {
+    this.id = id;
+    this.name = n;
+    this.closeContact = cc;
+    this.grades = g; 
+  }
+
+
+  // behavior
+  public boolean isCloseContact() {
+    // create method that updates the status of a student being close contact
+    // chances are, the student may have a different status any other day
+
+  }
+
+  // set the valye of close contact
+  public void setCloseContact(boolean value) {
+
+  }
+
+  // see if they're failing in class
+  public boolean isFailing() {
+    // average less than
+  }
+
+  // get student's name
+  public String getName() {
+
+  }
 }
