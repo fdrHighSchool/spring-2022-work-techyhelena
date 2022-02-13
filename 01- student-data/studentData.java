0c00c778a0ccb6f7bb6d8eb46e9f363631d41d39
Helen Zhou <fdr.helenzhou@gmail.com>
2022-02-13 17:03:06 -0500

    completed all instances and revised methods

--- 01- student-data/studentData.java
+++ 01- student-data/studentData.java
@@ -1,10 +1,10 @@
 public class studentData {
     public static void main(String[] args) {
       // student 1
-      // String id1 = "1234567";
-      // String name1 = "Jimmy Smith";
-      // boolean cc1 = true;
-      // int[] grades1 = {90, 80, 95, 90, 100};
+      String id1 = "1234567";
+      String name1 = "Jimmy Smith";
+      boolean cc1 = true;
+      int[] grades1 = {90, 80, 95, 90, 100};
   
       // student 2
       String id2 = "9876543";
@@ -33,18 +33,55 @@ public class studentData {
       // creating instances of the Student class
       Student student1 = new Student("9876543", "Jimmy Smith", true, new int[] {90, 80, 95, 90, 100});
       Student student2 = new Student("1234567", "Maria Hernandez", false, new int[] {95, 100, 100, 90, 90});
+      Student student3 = new Student("1357911", "Jamal Jenkins", false, new int[] {85, 85, 95, 100, 90});
+      Student student4 = new Student("2468101", "Ramiz Ahmed", true, new int[] {90, 100, 75, 85, 90});
+      Student student5 = new Student("1928374", "Michael Green", true, new int[] {80, 80, 65, 55, 90});
   
       // calling getter methods to display data
       System.out.println(student1.getName() + "'s average is: " + student1.getAverage());
       System.out.println(student2.getName() + "'s average is: " + student2.getAverage());
+
+      // getting student IDs
+      System.out.println("Student 1's ID is: " + student1.getID());
+      System.out.println("Student 5's ID is: " + student5.getID());
+
+
   
       // decision making based off of data attached to the object
-      if (student1.getCloseContact()) {
-        System.out.println("Please report to the auditorium for your take-home COVID test");
+      // if (student1.getCloseContact()) {
+      //   System.out.println("Please report to the auditorium for your take-home COVID test");
+      // }
+      // else {
+      //   System.out.println("You are not a close contact, keep masking! Get boosted!");
+      // }
+
+      // changing the status of close contact 
+      
+      if(student1.setCloseContact(false)) {
+        System.out.println(student1.getName() + ", You're now a close contact, please report to the auditorium for your take-home COVID test");
       }
+      
+      if(student2.setCloseContact(false)) {
+        System.out.println(student2.getName() + ", You're now a close contact, please report to the auditorium for your take-home COVID test");
+      }
+
+      if(student3.setCloseContact(false)) {
+        System.out.println(student3.getName() + ", You're now a close contact, please report to the auditorium for your take-home COVID test");
+      }
+
+      if(student4.setCloseContact(false)) {
+        System.out.println(student4.getName() + ", You're now a close contact, please report to the auditorium for your take-home COVID test");
+      }
+
+      if(student5.setCloseContact(false)) {
+        System.out.println(student4.getName() + ", You're now a close contact, please report to the auditorium for your take-home COVID test");
+      }
+
       else {
         System.out.println("You are not a close contact, keep masking! Get boosted!");
       }
+      
+      
     } // end main method
   
   } // end class
