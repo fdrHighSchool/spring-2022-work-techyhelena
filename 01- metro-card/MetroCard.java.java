40ccfd8e6e88d8ff2da762ef95b10f8ff01ccfc3
Helen Zhou <fdr.helenzhou@gmail.com>
2022-02-18 09:35:05 -0500

    changes

---
+++ 01- metro-card/MetroCard.java.java
@@ -0,0 +1,35 @@
+public class Tester {
+    private double time; 
+    private double fee; 
+    private double balance; 
+    private String status; 
+    private int rides;
+
+
+    // constructors 
+    public Tester(double t, double f, String s, int r) { // student card
+        this.time = t;
+        this.fee = f;
+        this.status = s;
+        this.rides = r;
+
+        // default info 
+        s = "Student";
+        r = 3; 
+    }
+
+    // normal card
+    public Tester(double t, double f, double b, double s) {
+
+    }
+
+
+    // behaviors 
+    public void swipe() {
+        if(!t.equals("5:30")) {
+
+        }
+    }
+
+}
+  
---
+++ 01- metro-card/Tester.java.java
@@ -0,0 +1,5 @@
+public class MetroCard {
+    public static void main(String[] args) {
+        
+    }
+}
