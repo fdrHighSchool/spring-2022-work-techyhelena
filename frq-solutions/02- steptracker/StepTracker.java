4bfc8ad31164cd77fb2c1acec60a0891fc5b78a5
Helen Zhou <fdr.helenzhou@gmail.com>
2022-03-02 09:18:45 -0500

    organizing files

---
+++ frq-solutions/02- steptracker/StepTracker.java
@@ -0,0 +1,11 @@
+public class StepTracker {
+    private int minDays; 
+    private int steps;
+    private int finalDays; 
+
+    public StepTracker(int min) {
+        this.minDays = min; 
+    }
+
+    
+}
