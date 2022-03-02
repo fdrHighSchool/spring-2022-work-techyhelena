ac5102aeab62239a0d3604b6cd0c5e1c526b606a
Helen Zhou <fdr.helenzhou@gmail.com>
2022-03-02 09:19:20 -0500

    cleaning files

--- 01- metro-card/MetroCard.java
+++ 01- metro-card/MetroCard.java
@@ -1,36 +1,25 @@
 public class MetroCard {
     // instance variables
-    // private String status;
-    // private double balance;
     private int rides;
-    // private double fee;
+    private String id; 
     private String time;
 
 
     // constructors
-    // public MetroCard(String s, double b) {
-    //     this.status = s;
-    //     this.balance = b;
-    // }
-
-    public MetroCard(String t) {
+    public MetroCard() {
         // this.status = s;
-        this.time = t;
         this.rides = 3;
+        this.id = "34587902";
     }
 
-    public void swipe() {
+    public void swipe(String t) {
         if(this.rides == 0) { //if student has no rides
             System.out.println("Insufficent rides available.");
 
         }
-        else if(timeParts(this.time) < 530 || timeParts(this.time) > 2030 ) { // time limit during 5:30 AM to 8:30 PM
+        else if(timeParts(t) < 530 || timeParts(t) > 2030 ) { // time limit during 5:30 AM to 8:30 PM
             System.out.println("You may not use your card during these hours.");
             this.rides = 0;
-        }
-
-        else if(timeParts(this.time) == 00) { // if midnight refresh # of rides
-            this.rides = 3;
         }
 
         else {
@@ -38,26 +27,27 @@ public class MetroCard {
             System.out.println("You have " + this.rides + " rides remaining.");
         }
 
+    }
 
 
-    } // end swipe()
-
-    // public double getBalance() {
-    //     return this.balance;
-    // }
+    //mutator method 
+    // start of a new day 
+    public void refresh() {
+        if(timeParts(this.time) == 530) { 
+            this.rides = 3;
+        }
+    }
 
     public int getRides() {
         return this.rides;
     }
-
 
     public int timeParts(String t) {
         this.time = t;
         int colon = t.indexOf(":");
 
         int hour = Integer.parseInt(t.substring(0, colon));
-        int min = Integer.parseInt(t.substring(colon + 1, 4));
-
+        int min = Integer.parseInt(t.substring(colon + 1));
 
         return hour + min;
     }
@@ -67,15 +57,14 @@ public class MetroCard {
         int colon = this.time.indexOf(":");
 
         int hour = Integer.parseInt(this.time.substring(0, colon));
-        int min = Integer.parseInt(this.time.substring(colon + 1, 4));
+        int min = Integer.parseInt(this.time.substring(colon + 1));
         return "hour: " + hour + " min: " + min;
-
     }
 
-    /*
+    
      public String toString() {
-         return
+         return "There is " + this.rides + " rides remaining in card number " + this.id;
      }
-    */ 
+    
 
 }
--- 01- metro-card/Tester.java
+++ 01- metro-card/Tester.java
@@ -1,11 +1,12 @@
 public class Tester {
     public static void main(String[] args) {
-        MetroCard studentCard = new MetroCard("20:00");
+        MetroCard studentCard = new MetroCard();
 
-        studentCard.swipe();
+        studentCard.swipe("20:00");
 
         System.out.println(studentCard.getRides());
         System.out.println(studentCard.getTime());
+        System.out.println(studentCard);
 
     }
 }
