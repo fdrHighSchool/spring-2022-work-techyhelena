ebceb8e2d32d5c4d92d0b3be41cdce4884cb1bc9
Helen Zhou <fdr.helenzhou@gmail.com>
2022-03-02 23:01:39 -0500

    added

--- frq-solutions/02- steptracker/StepTracker.java
+++ frq-solutions/02- steptracker/StepTracker.java
@@ -1,10 +1,25 @@
 public class StepTracker {
-    private int minDays; 
+    private int minSteps; 
     private int steps;
-    private int finalDays; 
+    private int days; 
 
     public StepTracker(int min) {
-        this.minDays = min; 
+        this.minSteps = min; 
+    }
+
+    public void addDailySteps(int steps) {
+        // accumlates info about steps, in readings taken once per day
+    }
+
+    public int activeDays(int days) {
+        // returns the number of active days
+        return 0;
+    }
+
+
+    public double averageSteps() {
+        // divide the total number of steps taken by the number of days tracked
+        return 0.0;
     }
 
     
