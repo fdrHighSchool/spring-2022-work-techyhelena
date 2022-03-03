4a17eafa7bd5164036119e57bf3e8915efbdd8f9
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2022-03-03 08:45:18 -0500

    frq

--- frq-solutions/02- steptracker/StepTracker.java
+++ frq-solutions/02- steptracker/StepTracker.java
@@ -1,26 +1,39 @@
 public class StepTracker {
-    private int minSteps; 
-    private int steps;
-    private int days; 
+    private int minSteps; // used for the new object
+    private int totalSteps; // used to calculate the average
+    private int totalDays;  // used to calculate the average
+    private int activeDays; //updated if it is greater than or equal to 10,000 steps
 
     public StepTracker(int min) {
-        this.minSteps = min; 
+        this.minSteps = min;
+        this.totalSteps = 0;
+        this.totalDays = 0;
+        this.activeDays = 0;
     }
 
     public void addDailySteps(int steps) {
         // accumlates info about steps, in readings taken once per day
+
+
     }
 
-    public int activeDays(int days) {
+    public int activeDays() {
         // returns the number of active days
-        return 0;
+        
     }
 
 
     public double averageSteps() {
         // divide the total number of steps taken by the number of days tracked
-        return 0.0;
+        // if days and steps are 0
+        if(numDays == 0) {
+          return 0.0;
+        }
+
+        else {
+          return (double)(totalSteps) / totalDays;
+        }
     }
 
-    
+
 }
--- frq-solutions/02- steptracker/Tester.java
+++ frq-solutions/02- steptracker/Tester.java
@@ -1,5 +1,5 @@
 public class Tester {
     public static void main(String[] args) {
-        
+        StepTracker tr = new StepTracker(10000);
     }
 }
