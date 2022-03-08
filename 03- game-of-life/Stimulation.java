095077c7eada8ea2aa8dab1e42975fc232b0cfdc
Helen Zhou <helenzhou@AC1MWK19GJC9H.local>
2022-03-08 08:48:05 -0500

    changes

--- 03- game-of-life/Stimulation.java
+++ 03- game-of-life/Stimulation.java
@@ -1,6 +1,7 @@
 public class Stimulation {
     public static void main(String[] args) {
         // make 2d array that stores random 0 and 1s (true and false basically)
+        int[][] grid = new String[10][10];
     }
-    
+
 }
