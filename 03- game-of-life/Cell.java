97ec40515196fde6d1a9fa28b0cdcb4456b3a78c
Helen Zhou <helenzhou@AC1MWK19GJC9H.local>
2022-03-09 08:47:47 -0500

    .

--- 03- game-of-life/Cell.java
+++ 03- game-of-life/Cell.java
@@ -1,11 +1,16 @@
 public class Cell {
-    /* RULES 
-    reproduction happens with exactly three neighbors 
+    /* RULES
+    reproduction happens with exactly three neighbors
     death happens when there are fewer than 2 or greater than 3 neighbors
     */
-    private boolean status; // true - alive .. false - dead
+    private int status; // 0 and 1 
 
     // constructor (blueprint)
-    
+    public Cell(int s) {
+      this.status;
+    }
+
+    public int
+
 
 }
--- 03- game-of-life/Stimulation.java
+++ 03- game-of-life/Stimulation.java
@@ -1,7 +1,22 @@
+import java.util.*;
 public class Stimulation {
     public static void main(String[] args) {
         // make 2d array that stores random 0 and 1s (true and false basically)
-        int[][] grid = new String[10][10];
+        int[][] grid = new String[10][10]; // col by row
+        Scanner s = new Scanner(System.in);
+
+        // make printBoard method
+        public static void printBoard(String[][] grid) {
+          for(int r = 0; r < arr.length; r++) {
+            for(int c = 0; c < arr[0].length; c++) {
+              grid[r][c] = "";
+            }
+          }
+        }
+        // make updateBoard method (next gen)
+        String input = s.nextLine(); // to update screen
+        
+
     }
 
 }
