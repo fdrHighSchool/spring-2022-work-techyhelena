f17af8b9e160a22d4e9f44a888ea4fc07e458b8a
Helen Zhou <helenzhou@AC1MWK19GJC9H.local>
2022-03-11 08:46:49 -0500

    fix null val

--- 03- game-of-life/Cell.java
+++ 03- game-of-life/Cell.java
@@ -3,17 +3,20 @@ public class Cell {
     reproduction happens with exactly three neighbors
     death happens when there are fewer than 2 or greater than 3 neighbors
     */
-    private int status; // 0 and 1 
+    private int status; // 0 and 1
 
     // constructor (blueprint)
     public Cell(int s) {
       this.status = s;
     }
 
-    // getter methods 
+    // getter methods
     public int getStatus() {
       return this.status;
     }
+
+    // setter methods
+    // setStatus 
 
 
 }
